e99f84cda2cfc096631ae04db408b4b478be6c91
Jens Oknelid <jens.oknelid@gmail.com>
2019-03-07 13:45:58 +0100

    Logo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import fontAscii from 'font-ascii';
+
+const logo = `
+██████╗███████╗██╗   ██╗██╗███╗   ███╗██████╗ 
+██╔════╝██╔════╝██║   ██║██║████╗ ████║██╔══██╗
+██║     ███████╗██║   ██║██║██╔████╔██║██████╔╝
+██║     ╚════██║╚██╗ ██╔╝██║██║╚██╔╝██║██╔═══╝ 
+╚██████╗███████║ ╚████╔╝ ██║██║ ╚═╝ ██║██║     
+ ╚═════╝╚══════╝  ╚═══╝  ╚═╝╚═╝     ╚═╝╚═╝     
+`
+
+const fonts = [
+  'Acrobatic',
+  'Alpha',
+  'Avatar',
+  'Big',
+  'BigMoneyne',
+  'BigMoneynw',
+  'BigMoneyse',
+  'BigMoneysw',
+  'Blocks',
+  'Bulbhead',
+  'Cards',
+  'Chiseled',
+  'Crawford2',
+  'Crazy',
+  'DancingFont',
+  'Diagonal3d',
+  'Doh',
+  'Doom',
+  'EftiWall',
+  'Epic',
+  'Firefontk',
+  'FireFonts',
+  'FlowerPower',
+  'FunFace',
+  'FunFaces',
+  'Ghost',
+  'Graceful',
+  'Graffiti',
+  'Impossible',
+  'Isometric1',
+  'Isometric2',
+  'Isometric3',
+  'Isometric4',
+  'JSBracketLetters',
+  'LilDevil',
+  'Merlin1',
+  'Modular',
+  'Ogre',
+  'PatorjkCheese',
+  'PatorjkHeX',
+  'Rectangles',
+  'Slant',
+  'SlantRelief',
+  'Small',
+  'SmallIsometric1',
+  'SmallSlant',
+  'Soft',
+  'Standard',
+  'StarWars',
+  'SubZero',
+  'SwampLand',
+  'Sweet',
+  'Train',
+  'Twisted',
+  'Varsity',
+  'WetLetts'
+]
+//fonts.map(f => <div><p id={f} >{fontAscii('CSVimp', { typeface: f })}</p><p>TEXT: {f}</p></div>)
+
+const StyledRoot = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background-color: #cdcdcd;
+  display: flex;
+  flex-direction: column;
+  padding: 1em;
+`
+
+const StyledLogo = styled.p`
+  font-size: 10pt;
+  font-family: "Courier New", Monospace;
+  white-space: pre;
+  margin: 0;
+`
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <StyledRoot>
+        <StyledLogo>{logo}</StyledLogo>
+
+
+      </StyledRoot>
     );
   }
 }
